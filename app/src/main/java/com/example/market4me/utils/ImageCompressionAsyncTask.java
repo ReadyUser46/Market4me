12b26f96f77f0ab3cb0ae903d48d6365cc3a9c61
Tork_PC <tork.developer@tork.com>
2020-07-05 17:29:51 +0200

    commit from async test: Compression working. Compressed picture uploaded to firestore

---
+++ app/src/main/java/com/example/market4me/utils/ImageCompressionAsyncTask.java
@@ -0,0 +1,26 @@
+package com.example.market4me.utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.lang.ref.WeakReference;
+
+public abstract class ImageCompressionAsyncTask extends AsyncTask<String, Void, String> {
+
+    private WeakReference<Context> contextRef;
+
+    public ImageCompressionAsyncTask(Context context) {
+        contextRef = new WeakReference<>(context);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        if (strings.length == 0 || strings[0] == null) return null;
+
+        return CameraUtils.compressImage(contextRef.get(), strings[0]);
+    }
+
+    protected abstract void onPostExecute(String s);
+
+
+}
